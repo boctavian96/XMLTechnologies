237c440f92417ec2eb0dca8fb16f917142264938
boctavian96 <boctavian96@yahoo.com>
2019-10-12 14:11:16 +0300

    Config file.

---
+++ XMLTechnologies/src/main/java/octavian/xmltech/app/Config.java
@@ -0,0 +1,18 @@
+package octavian.xmltech.app;
+
+public final class Config {
+	
+	public static final String DATABASE_FILE = "database.xml";
+	
+	//---------------DATABASE TABLES--------------
+	public static final String AUTHOR = "author";
+	public static final String DEPARTMENT = "department";
+	public static final String PUBLICATION = "publication";
+	
+	//---------------DOM PATHS--------------------
+	
+	
+	private Config() {
+		//Do not instantiate.
+	}
+}
