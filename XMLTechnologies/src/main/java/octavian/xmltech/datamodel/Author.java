fed6093923eb1a64a7c32daedb58fce1ac2abcf1
boctavian96 <boctavian96@yahoo.com>
2019-10-12 13:05:48 +0300

    Created datamodel.

---
+++ XMLTechnologies/src/main/java/octavian/xmltech/datamodel/Author.java
@@ -0,0 +1,129 @@
+package octavian.xmltech.datamodel;
+
+import java.util.Arrays;
+
+public class Author{
+
+    private int id;
+    private String fName;
+    private String lName;
+    private String address;
+    private String mobile;
+    private int[] affiliations;
+    
+    public Author(int id, String fName, String lName, String address, String mobile, int[] affiliations){
+        super();
+        this.id = id;
+        this.fName = fName;
+        this.lName = lName;
+        this.address = address;
+        this.mobile = mobile;
+        this.affiliations = affiliations;
+    }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public int[] getAffiliations() {
+		return affiliations;
+	}
+
+	public void setAffiliations(int[] affiliations) {
+		this.affiliations = affiliations;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + Arrays.hashCode(affiliations);
+		result = prime * result + ((fName == null) ? 0 : fName.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lName == null) ? 0 : lName.hashCode());
+		result = prime * result + ((mobile == null) ? 0 : mobile.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (!Arrays.equals(affiliations, other.affiliations))
+			return false;
+		if (fName == null) {
+			if (other.fName != null)
+				return false;
+		} else if (!fName.equals(other.fName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lName == null) {
+			if (other.lName != null)
+				return false;
+		} else if (!lName.equals(other.lName))
+			return false;
+		if (mobile == null) {
+			if (other.mobile != null)
+				return false;
+		} else if (!mobile.equals(other.mobile))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", fName=" + fName + ", lName=" + lName + ", address=" + address + ", mobile="
+				+ mobile + ", affiliations=" + Arrays.toString(affiliations) + "]";
+	}
+    
+    
+
+}
---
+++ XMLTechnologies/src/main/java/octavian/xmltech/datamodel/Publication.java
@@ -0,0 +1,139 @@
+package octavian.xmltech.datamodel;
+
+import java.util.Arrays;
+
+public class Publication {
+	private int id;
+	private String type;
+	private String year;
+	private int[] authors;
+	private String ISBN;
+	private String URL;
+	private int citations;
+	
+	public Publication(int id, String type, String year, int[] authors, String iSBN, String uRL, int citations) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.year = year;
+		this.authors = authors;
+		ISBN = iSBN;
+		URL = uRL;
+		this.citations = citations;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public int[] getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(int[] authors) {
+		this.authors = authors;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getURL() {
+		return URL;
+	}
+
+	public void setURL(String uRL) {
+		URL = uRL;
+	}
+
+	public int getCitations() {
+		return citations;
+	}
+
+	public void setCitations(int citations) {
+		this.citations = citations;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ISBN == null) ? 0 : ISBN.hashCode());
+		result = prime * result + ((URL == null) ? 0 : URL.hashCode());
+		result = prime * result + Arrays.hashCode(authors);
+		result = prime * result + citations;
+		result = prime * result + id;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + ((year == null) ? 0 : year.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Publication other = (Publication) obj;
+		if (ISBN == null) {
+			if (other.ISBN != null)
+				return false;
+		} else if (!ISBN.equals(other.ISBN))
+			return false;
+		if (URL == null) {
+			if (other.URL != null)
+				return false;
+		} else if (!URL.equals(other.URL))
+			return false;
+		if (!Arrays.equals(authors, other.authors))
+			return false;
+		if (citations != other.citations)
+			return false;
+		if (id != other.id)
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		if (year == null) {
+			if (other.year != null)
+				return false;
+		} else if (!year.equals(other.year))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Publication [id=" + id + ", type=" + type + ", year=" + year + ", authors=" + Arrays.toString(authors)
+				+ ", ISBN=" + ISBN + ", URL=" + URL + ", citations=" + citations + "]";
+	}
+	
+}
